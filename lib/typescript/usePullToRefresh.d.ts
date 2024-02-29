cb9083a823a15dde0681a5bd129e377698189ae8
Dave Gray <dave@davegray.ca>
2024-02-28 17:59:31 -0700

    chore: push lib
    Signed-off-by: Dave Gray <dave@davegray.ca>

---
+++ lib/typescript/usePullToRefresh.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import type { Options } from './types';
+export declare function usePullToRefresh({ onRefresh, debugMode }: Options): {
+    isRefreshing: boolean;
+    refreshController: {
+        refreshControl: JSX.Element;
+    };
+};
+//# sourceMappingURL=usePullToRefresh.d.ts.map
---
+++ lib/typescript/utils.d.ts
@@ -0,0 +1,4 @@
+import type { ListReference } from './types';
+export declare function isVirtualizedList(x: ListReference): boolean | null;
+export declare function isSectionList(x: ListReference): boolean | null;
+//# sourceMappingURL=utils.d.ts.map

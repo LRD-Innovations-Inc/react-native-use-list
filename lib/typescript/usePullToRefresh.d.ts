1e835a09a13f1d32ee6af3f3d8ab483617e1ef1c
Dave Gray <dave@davegray.ca>
2024-02-28 18:02:18 -0700

    Revert "chore: push lib"
    This reverts commit cb9083a823a15dde0681a5bd129e377698189ae8.

--- lib/typescript/usePullToRefresh.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import type { Options } from './types';
-export declare function usePullToRefresh({ onRefresh, debugMode }: Options): {
-    isRefreshing: boolean;
-    refreshController: {
-        refreshControl: JSX.Element;
-    };
-};
-//# sourceMappingURL=usePullToRefresh.d.ts.map
--- lib/typescript/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ListReference } from './types';
-export declare function isVirtualizedList(x: ListReference): boolean | null;
-export declare function isSectionList(x: ListReference): boolean | null;
-//# sourceMappingURL=utils.d.ts.map
